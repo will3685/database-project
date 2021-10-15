ab27aa1674e6475ee15ac4e9bd0ddb67060cf824
Will Theard <lavardwilltheard@yahoo.com>
2021-10-14 21:30:53 -0300

    added first table

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import *
 from .views import *
+
+
 class UserClientSerializer(serializers.ModelSerializer):
   class Meta:
     model = UserClient
